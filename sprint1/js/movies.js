22a4df452278f723dd1607889b3ed9de65e814e0
leonarderacagni <leonarderacagni@gmail.com>
2024-04-03 05:11:25 -0300

    sprint 1 javascript

---
+++ sprint1/js/movies.js
@@ -0,0 +1,20 @@
+let contenedorDePeliculas = document.getElementById("container")
+let fragmento = new DocumentFragment();
+
+let crearCadaCarta = function (objetoEntero) {
+    return `
+        <img class="" src="${objetoEntero.image}" alt="${objetoEntero.title}">
+        <h3 class="text-3xl mx-2 my-1 font-medium">${objetoEntero.title}</h3>
+        <h4 class=" mx-2 my-1">${objetoEntero.tagline}</h4>
+        <p class=" mx-2 my-2 text-clip overflow-hidden">${objetoEntero.overview}</p>
+    `
+}
+
+for (const iterator of peliculas) {
+    let divCard = document.createElement("div");
+    divCard.className = "h-96 w-96 flex flex-col m-3 bg-[#6D38E0] border-solid hover:border-2 border-[#D2CCFF]" //clases de estilos de tailwind
+    divCard.innerHTML += crearCadaCarta(iterator);
+    fragmento.appendChild(divCard);
+}
+
+contenedorDePeliculas.appendChild(fragmento);
